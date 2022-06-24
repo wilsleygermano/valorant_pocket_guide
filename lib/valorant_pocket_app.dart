b0acb20df92030d1e8cd95c649502a841b4a536c
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-06-24 15:12:27 -0300

    API calls setup done

--- lib/valorant_pocket_app.dart
+++ lib/valorant_pocket_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:valorant_pocket_guide/presentation/view/home_page.dart';
+import 'package:valorant_pocket_guide/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
