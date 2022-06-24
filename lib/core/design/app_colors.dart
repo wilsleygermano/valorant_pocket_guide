4a7f778a4428564860facd37deb1558b21df1063
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-06-24 14:56:43 -0300

    App colors and fonts selected.

---
+++ lib/core/design/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color mainRedishColor = Color(0xFFfa4454);
+  static const Color mainRedishDarkerColor = Color(0xFFdc3d4b);
+  static const Color greenColor = Color(0xFF042e27);
+  static const Color brownColor = Color(0xFFb78460);
+  static const Color nudeColor = Color(0xFFb38c8f);
+  static const Color navalBlue = Color(0xFF364966);
+  static const Color babyPowder = Color(0xFFfefefa);
+  static const Color jetBlack = Color(0xFF0E0E10);
+  static const Color transparent = Colors.transparent;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
