27eb50e0b296ff2e911ee10cece50b21668a5940
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-06-27 09:36:12 -0300

    Fetching Agents list data tested and working

--- lib/core/constants/api_routes.dart
+++ lib/core/constants/api_routes.dart
@@ -4,8 +4,7 @@ class ApiRoutes {
       "https://valorant-api.com/v1/agents/";
   static const String mapsApiListCall = "https://valorant-api.com/v1/maps";
   static const String singleMapApiCall = "https://valorant-api.com/v1/maps/";
-  static const String agentsApiList = "https://valorant-api.com/v1/agents";
-  static const String weaponsApiListCall =
+    static const String weaponsApiListCall =
       "https://valorant-api.com/v1/weapons";
   static const String weaponsSkinsApiListCall =
       "https://valorant-api.com/v1/weapons/skins";
--- test/home/data/data_source/api_call_agents_list_data_source_test.dart
+++ test/home/data/data_source/api_call_agents_list_data_source_test.dart
@@ -1,11 +1,64 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:modular_test/modular_test.dart';
+import 'package:valorant_pocket_guide/app_module.dart';
+import 'package:valorant_pocket_guide/core/adapters/remote_client.dart';
+import 'package:valorant_pocket_guide/core/adapters/remote_client_error.dart';
 import 'package:valorant_pocket_guide/core/features/home/data/data_source/call_agents_list_data_source.dart';
-import 'package:valorant_pocket_guide/core/features/home/home_module.dart';
+import 'package:valorant_pocket_guide/core/generics/api_response.dart';
+import 'package:valorant_pocket_guide/core/generics/resource.dart';
 
 void main() {
-group("Api Call Agents List Test", () {
-  initModule(HomeModule(), replaceBinds: [Bind.instance<CallAgentsListDataSource>(ApiCallAgentsListDataSource())]);
-});  
+  // intancio uma classe "fake" para teste (criada ao fim do documento)
+  final stub = RemoteClientStub();
+
+  group(
+    "Api Call Agents List Test",
+    () {
+      // injeção criada pelo modular
+      initModule(
+        AppModule(),
+        replaceBinds: [Bind.instance<RemoteClient>(stub)],
+      );
+
+      // unidade que desejo testar
+      final testingUnit = ApiCallAgentsListDataSource();
+
+      test(
+        "When statusCode != 200 then should return an error",
+        () async {
+          stub.apiResponseToUse = ApiResponse(statusCode: 400);
+          final result = await testingUnit.returnAgentsList();
+          expect(result.error, isNot(null));
+          expect(result.data, isNull);
+          expect(result.status, Status.failed);
+        },
+      );
+      test(
+        "When statusCode == 200 then should return an error",
+        () async {
+          stub.apiResponseToUse =
+              ApiResponse(statusCode: 200, data: {'data': 'data'});
+          final result = await testingUnit.returnAgentsList();
+          expect(result.data, isNot(null));
+          expect(result.error, isNull);
+          expect(result.status, Status.success);
+        },
+      );
+    },
+  );
+}
+
+// "mocko" uma classe que já existe para tentar obter sus hipóteses, implementando uma
+// classe que já existe.
+class RemoteClientStub implements RemoteClient {
+  // crio uma variável que se retorna o tipo esperado pela função a ser testada.
+  ApiResponse<Map<String, dynamic>, RemoteClientError>? apiResponseToUse;
+
+  @override
+  Future<ApiResponse<Map<String, dynamic>, RemoteClientError>> get(
+      String url) async {
+        // aqui passo a variável acima criada.
+    return apiResponseToUse!;
+  }
 }
